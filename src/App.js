cfe1425b856f9d8c42925356647c903a2a99fa99
Julian <j.henry.b.orr@gmail.com>
2019-06-05 16:27:32 -0400

    Resume First Commit

---
+++ src/App.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    
+    <div className="App">
+
+    <div className="leftPanel">
+    <header className="title">Skills</header>
+    
+    <ul class= "skillList">
+    <li>Microsoft Office</li>
+    <li>Programming</li>
+    <li>Test Driven Development</li>
+    <li>Git</li>
+    <li>SQL</li>
+    <li>Ruby</li>
+    <li>C#</li>
+    <li>React</li>
+    <li>JavaScript</li>
+
+    </ul>
+    </div>
+    
+      <header className="App-header">
+      <h4>
+          Julian Henry Bordwell Orr
+        </h4>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          MovieDB Project
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
